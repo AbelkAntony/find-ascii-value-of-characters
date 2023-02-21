9d79d7033088a05b4005230e941d5e7baf097515
ABEL K ANTONY <abelkantony98@gmail.com>
2023-02-21 11:28:20 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	while(true)
+	{
+		int ascii;
+		char ch;
+		cout<<"\nENTER THE CHARACTER : ";
+		cin>>ch;
+		ascii = int(ch);
+		cout<<"ASCII VALUE : "<<ascii;
+		
+	}
+}
